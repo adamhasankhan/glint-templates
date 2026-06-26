be97466d7c19056b18e40e3174444ae510f60467
Adam Khan <khan1@kenyon.edu>
2026-06-26 14:55:49 -0400

    updated glint model comparison run settings for a faster configuration

--- run_glint_pe.sh
+++ run_glint_pe.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-{ time python3 glint_pe.py --npool 20 --event GW231226_101520 --label GW231226_101520-perturber_param_test --perturber_params --rand 1082632 --maxmcmc 8192 --nact 20 --nlive 2048 &> glint_pe.log; } 2> time.log
+{ time python3 glint_pe.py --npool 10 --event GW231226_101520 --label GW231226_101520-perturber_param_test --perturber_params --rand 1082632 --maxmcmc 6144 --nact 15 --nlive 1024 &> glint_pe.log; } 2> time.log
